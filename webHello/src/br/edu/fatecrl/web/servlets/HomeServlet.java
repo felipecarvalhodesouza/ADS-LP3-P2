a73d6d0c87522739a148c569091ced6e09a3107b
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2019-06-02 12:31:58 -0300

    Contador de requisições na página

--- webHello/src/br/edu/fatecrl/web/servlets/HomeServlet.java
+++ webHello/src/br/edu/fatecrl/web/servlets/HomeServlet.java
@@ -9,50 +9,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HomeServlet
- */
 @WebServlet(name = "HomeServlet", urlPatterns = {"/home"})
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		processRequest(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	
+	int contador=0;
 	
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setContentType("text/html;charset=UTF-8");
-		try (PrintWriter out = response.getWriter()){
+		PrintWriter out = response.getWriter();
+		try{
 			out.println("<!DOCTYPE html>");
 			out.println("<html>");
 			out.println("<head>");
 			out.println("<title>Meu primeiro Servlet</title>");
 			out.println("</head>");
 			out.println("<body>");
-			out.println("<h1>Olá, mundo do JavaEE!!</h1>");
+			out.println("<h1>Quantidade de requisições (visitas): " + contador++ + "</h1>");
 			out.println("</body>");
 			out.println("</html>");
 		}
+		finally {
+			out.close();
+		}
+	}
+	
+    public HomeServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
 	}
 
 }
